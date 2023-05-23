7e0a21d068cc48a7978c330150ac05a2f02892a7
kulotsystems <kulotsystems@gmail.com>
2023-05-23 12:32:11 +0800

    Update title of minor awards generator.

--- app/crud/minor-awards/index.php
+++ app/crud/minor-awards/index.php
@@ -36,7 +36,7 @@
 </head>
 <body>
     <div class="container py-5">
-        <h2 class="fw-bolder">Binibining San Vicente (Buhi, Camarines Sur) 2023</h2>
+        <h2 class="fw-bolder">Miss Kaogma 2023 Screening</h2>
     </div>
     <div>
         <table>
